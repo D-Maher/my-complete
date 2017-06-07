4d50871e1dcfcaf2d8c5484597170df8016e8d68
Danny Maher <dmaher724@gmail.com>
2017-06-07 11:24:24 -0500

    Refactor ClientApp.jsx with JSX

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -5,21 +5,38 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // shorthand for calling React.createElement() below
-const ce = React.createElement;
+// const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
+  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title)); // this is the same as below, in fact, below gets transpiled to pretty much this
 
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'yellowgreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'greenyellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'limegreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'aquamarine' })
+  const style = { color: props.color };
+  return (
+    <div>
+      <h1 style={style}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  // return ce(
+  //   'div',
+  //   { id: 'my-first-component' },
+  //   ce(MyTitle, { title: 'Game of Thrones', color: 'skyblue' }),
+  //   ce(MyTitle, { title: 'Stranger Things', color: 'greenyellow' }),
+  //   ce(MyTitle, { title: 'Rick and Morty', color: 'limegreen' }),
+  //   ce(MyTitle, { title: 'Silicon Valley', color: 'aquamarine' })
+  // );
+
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of Thrones" color="skyblue" />
+      <MyTitle title="Stranger Things" color="greenyellow" />
+      <MyTitle title="Rick and Morty" color="limegreen" />
+      <MyTitle title="Silicon Valley" color="aquamarine" />
+    </div>
+  );
+};
+
+// render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
