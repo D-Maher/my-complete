e321a30e73a87254c9adf86fa2f0097a451f9509
Danny Maher <dmaher724@gmail.com>
2017-06-07 15:48:46 -0500

    Add routes for Landing and Search

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,9 +3,18 @@
 import React from 'react';
 // only include render from 'react-dom' since its the only thing that we need from ReactDOM
 import { render } from 'react-dom';
-// import the Landing component from Landing.jsx
+// include HashRouter and Route from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom';
+// import other components from js/ directory
 import Landing from './Landing';
+import Search from './Search';
 
-const App = () => <div className="app" />;
+const App = () =>
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
+    </div>
+  </HashRouter>;
 
 render(<App />, document.getElementById('app'));
