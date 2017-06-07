6ae94aa11d760d359d2cfc9d7b339d25b3091882
Danny Maher <dmaher724@gmail.com>
2017-06-07 14:11:03 -0500

    Remove introductory React code to make way for app building

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,39 +4,13 @@ import React from 'react';
 // below we are only including render from 'react-dom' since its the only thing that we need from ReactDOM
 import { render } from 'react-dom';
 
-// shorthand for calling React.createElement() below
-// const ce = React.createElement;
-
-const MyTitle = function(props) {
-  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title)); // this is the same as below, in fact, below gets transpiled to pretty much this
-
-  const style = { color: props.color };
-  return (
-    <div>
-      <h1 style={style}>{props.title}</h1>
+const App = () =>
+  <div className="app">
+    <div className="landing">
+      <h1>React Vids</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
     </div>
-  );
-};
+  </div>;
 
-const MyFirstComponent = function() {
-  // return ce(
-  //   'div',
-  //   { id: 'my-first-component' },
-  //   ce(MyTitle, { title: 'Game of Thrones', color: 'skyblue' }),
-  //   ce(MyTitle, { title: 'Stranger Things', color: 'greenyellow' }),
-  //   ce(MyTitle, { title: 'Rick and Morty', color: 'limegreen' }),
-  //   ce(MyTitle, { title: 'Silicon Valley', color: 'aquamarine' })
-  // );
-
-  return (
-    <div id="my-first-component">
-      <MyTitle title="Game of Thrones" color="skyblue" />
-      <MyTitle title="Stranger Things" color="greenyellow" />
-      <MyTitle title="Rick and Morty" color="limegreen" />
-      <MyTitle title="Silicon Valley" color="aquamarine" />
-    </div>
-  );
-};
-
-// render(ce(MyFirstComponent), document.getElementById('app'));
-render(<MyFirstComponent />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
