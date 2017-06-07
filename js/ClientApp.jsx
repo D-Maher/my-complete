12f751050a14000234d1c0e6fbcea8265d4dabb9
Danny Maher <dmaher724@gmail.com>
2017-06-07 14:50:31 -0500

    Rename app

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -7,7 +7,7 @@ import { render } from 'react-dom';
 const App = () =>
   <div className="app">
     <div className="landing">
-      <h1>React Vids</h1>
+      <h1>Reactflix</h1>
       <input type="text" placeholder="Search" />
       <a>or Browse All</a>
     </div>
