30dc25ab1995fc4ee77ea362d29a673d19e23488
Danny Maher <dmaher724@gmail.com>
2017-06-08 14:18:34 -0500

    Display each show title in an h3 on the Search page

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+// import preload data from data.json
+import preload from '../data.json';
 
-const Search = () => <h1>Hi, this is search!</h1>;
+// we can stick the following inside the Search component to see all of the data in data.json:
+// <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+
+const Search = () =>
+  <div className="search">
+    {preload.shows.map(show => <h3>{show.title}</h3>)}
+  </div>;
 
 export default Search;
